761faa2d80653e09fd9706d999cec832634d8e9b
Peng Ding <pding.dp@foxmail.com>
2025-04-13 22:12:17 -0500

    ✨ feat(toolregistry): add namespace support for tool registration
    - enable namespace handling in openapi and mcp tool registration
    - update pprint statements to display available tools and sub-registries for better visibility

--- examples/openai_toolregistry_mixed_math.py
+++ examples/openai_toolregistry_mixed_math.py
@@ -16,7 +16,9 @@ print("================ OpenAPI ================")
 
 
 async def async_register():
-    await openapi_registry.register_openapi_tools_async(openapi_spec_url)
+    await openapi_registry.register_openapi_tools_async(
+        openapi_spec_url, with_namespace=True
+    )
 
 
 OPENAPI_PORT = os.getenv(
@@ -28,7 +30,8 @@ OPENAPI_PORT = os.getenv(
     "OPENAPI_PORT", 8000
 )  # default OPENAPI_PORT 8000, change via environment variable
 asyncio.run(async_register())
-pprint(openapi_registry)
+pprint(openapi_registry.get_available_tools())
+pprint(openapi_registry._sub_registries)
 
 
 # ================ register MCP server (sync) ================
@@ -38,14 +41,16 @@ MCP_PORT = os.getenv(
 )  # default MCP_PORT 8000, change via environment variable
 mcp_registry = ToolRegistry("mcp_math")
 mcp_server_url = f"http://localhost:{MCP_PORT}/mcp/sse"
-mcp_registry.register_mcp_tools(mcp_server_url)
-pprint(mcp_registry)
+mcp_registry.register_mcp_tools(mcp_server_url, with_namespace=True)
+pprint(mcp_registry.get_available_tools())
+pprint(mcp_registry._sub_registries)
 
 # ================ mix registry ================
 print("================ MIXUP ================")
 mixed_registry = openapi_registry
 mixed_registry.merge(mcp_registry)
-pprint(mixed_registry)
+pprint(mixed_registry.get_available_tools())
+pprint(mixed_registry._sub_registries)
 
 # ================ testing ================
 # Set up OpenAI client
