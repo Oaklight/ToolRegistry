6116f0f89e5f9215dbd714bac5ecf9e446cc8536
Peng Ding <pding.dp@foxmail.com>
2025-04-27 20:12:19 -0500

    🔧 chore(version): bump version to 0.4.6.post2
    - update release version in conf.py
    - modify project version in pyproject.toml
    - update __version__ in __init__.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ project = "ToolRegistry"
 copyright = "2024-2025, Peng Ding"
 author = "Peng Ding"
 html_title = "ToolRegistry"
-release = "0.4.6.post1"
+release = "0.4.6.post2"
 
 import os
 import sys
